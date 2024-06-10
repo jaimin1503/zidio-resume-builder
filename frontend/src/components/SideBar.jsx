f9bc727dd5f7be7054d54c100e30208de78f3480
jaimin1503 <123434935+jaimin1503@users.noreply.github.com>
2024-06-10 17:50:18 +0530

    Hover effect on menuitems

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,18 +1,26 @@
 import MenuItem from "../smallcomponents/MenuItem";
-import contact from "./assets/contact.svg";
-import certification from "./assets/certification.svg";
-import Education from "./assets/Education.svg";
-import experience from "./assets/experience.svg";
-import personalInfo from "./assets/personalInfo.svg";
+import Contact from "./assets/Contact.jsx";
+import Certification from "./assets/Certification.jsx";
+import Education from "./assets/Education.jsx";
+import Experience from "./assets/Experience.jsx";
+import PersonalInfo from "./assets/PersonalInfo.jsx";
 
 export default function SideBar() {
+  const menuItems = [
+    { logo: PersonalInfo, text: "Personal Information" },
+    { logo: Education, text: "Education" },
+    { logo: Experience, text: "Experience" },
+    { logo: Contact, text: "Contact Information" },
+    { logo: Certification, text: "Award/Certification" },
+  ];
+
   return (
-    <div className=" rounded-2xl mx-10 absolute flex flex-col justify-center items-center bg-blue-200 w-[379px] h-[600px] top-44 shadow-lg ">
-      <MenuItem logo={personalInfo} text={"Pesonal Information"} />
-      <MenuItem logo={Education} text={"Education"} />
-      <MenuItem logo={experience} text={"Experience"} />
-      <MenuItem logo={contact} text={"Contact Information"} />
-      <MenuItem logo={certification} text={"Award/Certification"} />
+    <div className="rounded-2xl mx-10 absolute flex flex-col justify-center items-center bg-richblack-700 w-[379px] h-[600px] top-44 shadow-lg">
+      {menuItems.map((item, index) => (
+        <div key={index}>
+          <MenuItem logo={item.logo} text={item.text} />
+        </div>
+      ))}
     </div>
   );
 }
--- frontend/src/smallcomponents/MenuItem.jsx
+++ frontend/src/smallcomponents/MenuItem.jsx
@@ -1,8 +1,16 @@
-export default function MenuItem({ logo, text }) {
+import { useState } from "react";
+
+export default function MenuItem({ logo: Logo, text }) {
+  const [isHover, setIsHover] = useState(false);
+
   return (
-    <div className=" my-4 h-[70px] flex justify-start items-center rounded-xl w-[322px] bg-white text-blue-300 font-bold border-2 border-blue-300 cursor-pointer">
+    <div
+      onMouseOver={() => setIsHover(true)}
+      onMouseOut={() => setIsHover(false)}
+      className=" my-4 h-[70px] flex justify-start items-center rounded-xl w-[322px] bg-white hover:text-blue-400 font-bold border-2 border-white text-black hover:border-blue-400 cursor-pointer"
+    >
       <div className=" flex items-center ml-5">
-        <img src={logo} alt="logo" />
+        <Logo isHover={isHover} />
         <h1 className=" mx-4">{text}</h1>
       </div>
     </div>
