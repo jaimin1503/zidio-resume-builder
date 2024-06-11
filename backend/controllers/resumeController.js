a26afbbc4469095679d39768065fa0222dbf2f8d
Kausalya-s673 <164292535+Kausalya-s673@users.noreply.github.com>
2024-06-11 13:12:13 +0530

    Update resumeController.js

--- backend/controllers/resumeController.js
+++ backend/controllers/resumeController.js
@@ -53,3 +53,41 @@ export const createResume = async (req, res) => {
     res.status(500).json({ message: "Server error, please try again later." });
   }
 };
+<<<<<<< Updated upstream
+=======
+
+export const editResume = async (req, res) => {
+  try {
+    const resumeId = req.params.id;
+    const userId = req.user._id;
+
+    // Find the resume by ID
+    const resume = await Resume.findById(resumeId);
+
+    // Check if the resume exists
+    if (!resume) {
+      return res.status(404).json({ message: "Resume not found" });
+    }
+
+    // Check if the resume belongs to the authenticated user
+    if (resume.user.toString() !== userId) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    // Update resume fields with provided data
+    const updates = req.body;
+    Object.assign(resume, updates);
+
+    // Save the updated resume
+    const updatedResume = await resume.save();
+
+    // Return the updated resume in the response
+    res.status(200).json(updatedResume);
+  } catch (error) {
+    // Handle errors
+    console.error(error);
+    res.status(500).json({ message: "Server error, please try again later." });
+  }
+};
+//Kausalya
+>>>>>>> Stashed changes
