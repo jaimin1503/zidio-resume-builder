1d93ade428cfbcc3f25ff962ad30c473afd8979b
jaimin1503 <123434935+jaimin1503@users.noreply.github.com>
2024-06-11 12:50:38 +0530

    added edit resume controller

--- backend/routes/resumeRoutes.js
+++ backend/routes/resumeRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { auth } from "../middlewares/auth.js";
-import { createResume } from "../controllers/resumeController.js";
+import { createResume, editResume } from "../controllers/resumeController.js";
 
 const router = express.Router();
 
 router.post("/createResume", auth, createResume);
+router.put("/editRedume/:id", auth, editResume);
 
 export default router;
